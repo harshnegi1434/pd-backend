7f5588b27bdeb43059ed3962f318775149bb82fd
Harsh Negi <harshnegi2002@gmail.com>
2024-07-24 10:41:36 +0530

    CORS updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
-CORS(app, resources={r"/check-url": {"origins": "http://localhost:3000"}})
+CORS(app, resources={r"/check-url": {"origins": "https://phishdetective-git-main-harshnegi1434s-projects.vercel.app/"}})
 
 # Load the trained Gradient Boosting Classifier
 with open("models/model.pkl", "rb") as file:
